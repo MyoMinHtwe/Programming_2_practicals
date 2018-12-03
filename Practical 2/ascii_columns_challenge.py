a8c7dc4cc34389ab30776083ffbbd09d34cbcf96
MyoMinHtwe <myomin.htwe@my.jcu.edu.au>
2018-12-03 22:55:35 +0800

    Ascii column challenge. Practical 2

--- Practical 2/ascii_columns_challenge.py
+++ Practical 2/ascii_columns_challenge.py
@@ -2,5 +2,5 @@ def add_column():
     column = int(input("How many columns do you want to print? "))
     print("\nASCII table ({} columns)".format(column))
     for i in range (33, 127):
-        print(("{:>3}{:>2s}   ".format(i, chr(i)))* column)
+        print(("{:3}{:>2s}   ".format(i, chr(i)))* column)
 add_column()
